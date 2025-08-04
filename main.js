73e40261f3584a0972d881121b13297ab05f62de
leandro-ac <leandro.alvaresc@gmail.com>
2025-08-04 13:05:48 -0300

    add cria discos

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ eH1.textContent = "Olá mundo!";
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+for(let i=0; i<7; i++){
+	const novoDisco = criaDisco();
+	eTabuleiro.append(novoDisco);
+}
+
+function criaDisco(){
+	const novoDisco = document.createElement("div");
+	novoDisco.classList.add("disco");
+	return novoDisco;
+}
+
 function criaTabuleiro(){
     const novoTabuleiro = documento.createElement("div");
     novoTabuleiro.classList.add("tabuleiro");
