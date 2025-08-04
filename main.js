650765d998e20e0586e51f5cd4e9ed6a2c02e740
leandro-ac <leandro.alvaresc@gmail.com>
2025-08-04 12:56:50 -0300

    add cria tabuleiro

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const eH1 = document.querySelector("h1");
 eH1.textContent = "Olá mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+
+function criaTabuleiro(){
+    const novoTabuleiro = documento.createElement("div");
+    novoTabuleiro.classList.add("tabuleiro");
+    novoTabuleiro.style.background = "lightgreen";
+	novoTabuleiro.style.minHeight = "25px";
+    return novoTabuleiro;
+}
